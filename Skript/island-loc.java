1f5b07aa041c3ca1ae85414a2fb9cc43d279c50d
bnjrKemal <35656797+bnjrKemal@users.noreply.github.com>
2023-10-05 04:49:12 +0300

    Update island-loc.java

--- Skript/island-loc.java
+++ Skript/island-loc.java
@@ -1,25 +1,29 @@
 // Java
+public class Main {
+    
 	public static void main(String[] args) {
-        getFreeLocation(2, 1); //Kaç tane lokasyon verir formülü; (2f+1)^2
+        	getFreeLocation(1, 90); //Kaç tane lokasyon verir formülü; (2f+1)^2
 	}
 	
 	private static void getFreeLocation(int f, int k){
-	    for(int x = f*-1; x<=f; x++){
-            for(int z = f*-1; z<=f; z++){
-                //is air
-                /*return */System.out.println("(" + x * k + ", " + z * k + ")");
-            }
-        }
-        getFreeLocation(f++, k);
+		for(int x = f*-1; x<=f; x++){
+			for(int z = f*-1; z<=f; z++){
+                		//is air
+                		/*return */System.out.println("(" + x * k + ", " + z * k + ")");
+            		}
+        	}
+        	getFreeLocation(f++, k);
 	}
-
+	
 	private static void findMainLocation(Location location, int k){
-	    int hX = location.getX() + (k/2);
-	    int hZ = location.getZ() + (k/2);
-	    int x = hX - Math.mod(hX,  k);
-	    int z = hZ - Math.mod(hZ,  k);
-	    System.out.println(x + "," + z);
+        	int hX = location.getX() + (k/2);
+        	int hZ = location.getZ() + (k/2);
+        	int x = hX - Math.mod(hX,  k);
+        	int z = hZ - Math.mod(hZ,  k);
+        	System.out.println(x + "," + z);
 	}
+	
+}
 
 /* -> Output
 (-2, -2)
